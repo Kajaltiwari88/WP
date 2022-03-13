549f6de067ab73c31d56cf47419de8243b8c1b56
Kajaltiwari88 <98203922+Kajaltiwari88@users.noreply.github.com>
2022-03-13 21:48:07 +0530

    Add files via upload
    home -menu/footer

---
+++ footer.php
@@ -0,0 +1,22 @@
+ <!-- footer section-->
+ <section class="footer">
+        <div class="container text-center">
+            <div class="social text-center">
+                <ul>
+                    <li> <a href="#"><img src="https://img.icons8.com/color/48/000000/instagram-new--v2.png"/></a></li>
+                    <li> <a href="#"><img src="https://img.icons8.com/color-glass/48/000000/facebook.png"/></a></li>
+                    <li> <a href="#"><img src="https://img.icons8.com/color/48/000000/twitter--v2.png"/></a></li>
+                </ul>
+            </div>
+            <div class="rights">
+                <p class="p1">All rights reserved. Designed by Kajal, Anusree, Abhishek, Kamli</p>
+            </div>
+
+            <div class="clearfix">
+            </div>   
+            
+        </div>
+
+    </section>
+</body>
+</html>
---
+++ menu.php
@@ -0,0 +1,29 @@
+<?php include('config/confidential.php'); ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+   <!--Important to make website responsive -->
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Restaurant Website</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    <!-- Navbar section-->
+    <section class="navbar">
+        <div class="container">
+            <div class="logo">
+                <img src="images/kk.jpg" alt="Restaurant logo" class="img-responsive">
+            </div>
+            <div class="menu text-right">
+                <ul>
+                    <li> <a href="index.php">Home</li>
+                    <li> <a href="#">Contact</a></li>
+                    <li> <a href="index.html">Feedback</a></li>
+                </ul>
+            </div>
+            <div class="clearfix">
+            </div>          
+        </div>
+    </section>
